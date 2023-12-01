4f0401dc39b081f002736b8698d7c03c8a50c35d
Linda Meng <itsyuko0o1@gmail.com>
2023-12-01 15:05:03 -0800

    a

--- locationfind.js
+++ locationfind.js
@@ -21,7 +21,6 @@ function geoFindMe() {
     if (!navigator.geolocation) {
       status.textContent = "Geolocation is not supported by your browser";
     } else {
-      status.textContent = "Locating…";
       navigator.geolocation.getCurrentPosition(success, error);
     }
   }
