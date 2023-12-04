06a800a3ad3bc64448cf33a4809754f8d66c708d
Linda Meng <itsyuko0o1@gmail.com>
2023-12-04 15:55:27 -0800

    a

--- locationfind.js
+++ locationfind.js
@@ -1,36 +1,38 @@
 function geoFindMe() {
-    const status = document.getElementById("status");
-    const coords = document.getElementById("coords");
-  
-    function success(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = "Location tracking started";
-      coords.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = "Unable to retrieve your location";
-    }
-  
-    if (!navigator.geolocation) {
-      status.textContent = "Geolocation is not supported by your browser";
-    } else {
-      status.textContent = "Locating...";
-      // Use getCurrentPosition instead of watchPosition
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
+  const status = document.getElementById("status");
+  const coords = document.getElementById("coords");
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = "Location tracking started";
+    coords.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
   }
-  
-  // Call the geoFindMe function when the page loads
-  window.onload = function() {
-    let id = setInterval(geoFindMe, 1000); // call the function every 5 seconds
-    document.querySelector("#stop-tracking").addEventListener("click", function() {
-      clearInterval(id); // clear the interval when the button is clicked
-      status.textContent = "Location tracking stopped";
-      coords.textContent = "";
-    });
-  };
-  
-  
+
+  function error() {
+    status.textContent = "Unable to retrieve your location";
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    status.textContent = "Locating...";
+    // Add a parameter to specify the options for the geolocation request
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    return navigator.geolocation.watchPosition(success, error, options);
+  }
+}
+
+window.onload = function() {
+  let id = geoFindMe(); // store the watch ID in a variable
+  document.querySelector("#stop-tracking").addEventListener("click", function() {
+    navigator.geolocation.clearWatch(id); // clear the watch when the button is clicked
+    status.textContent = "Location tracking stopped";
+    coords.textContent = "";
+  });
+};
