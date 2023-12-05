ae93cfefe4de7d3ecb7eb024c76cb7723c3390b5
Linda Meng <itsyuko0o1@gmail.com>
2023-12-04 17:28:23 -0800

    a

--- locationfind.js
+++ locationfind.js
@@ -8,8 +8,8 @@ if ("geolocation" in navigator) {
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
 
-      // Update the element with the user's new location
-      showPosition(position);
+      // Update the map with the user's new location
+      console.log(`Latitude: ${lat}, longitude: ${lng}`);
     },
     // Error callback function
     function(error) {
@@ -21,3 +21,28 @@ if ("geolocation" in navigator) {
   // Geolocation is not supported by the browser
   console.error("Geolocation is not supported by this browser.");
 }
+
+// Define a function to get the user's location
+function getLocation() {
+  if (navigator.geolocation) {
+    // Use the same success and error callbacks as above
+    navigator.geolocation.watchPosition(showPosition, function(error) {
+      // Handle errors, e.g. user denied location sharing permissions
+      console.error("Error getting user location:", error);
+    });
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+// Define a function to show the position
+function showPosition(position) {
+  // Get the element where the coordinates will be displayed
+  const x = document.getElementById("coords");
+
+  // Update the element's innerHTML with the latitude and longitude
+  x.innerHTML = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
+}
+
+// Call the getLocation function when the page loads
+window.addEventListener("load", getLocation);
