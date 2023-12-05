a18791a3b79ecad382c0f93dd6d21589e6e9fe8b
Linda Meng <itsyuko0o1@gmail.com>
2023-12-04 16:38:51 -0800

    a

--- locationfind.js
+++ locationfind.js
@@ -1,59 +1,17 @@
-// Check if geolocation is supported by the browser
+
 if ("geolocation" in navigator) {
-  // Prompt user for permission to access their location
   navigator.geolocation.watchPosition(
-    // Success callback function
     function(position) {
       // Get the user's latitude and longitude coordinates
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
 
-      // Update the map with the user's new location
       console.log(`Latitude: ${lat}, longitude: ${lng}`);
     },
-    // Error callback function
     function(error) {
-      // Handle errors, e.g. user denied location sharing permissions
       console.error("Error getting user location:", error);
     }
   );
-} else {// Get a reference to the HTML element where the coordinates will be displayed
-  var coordsDiv = document.getElementById("coordsDiv");
-  
-  // Check if geolocation is supported by the browser
-  if ("geolocation" in navigator) {
-    // Prompt user for permission to access their location
-    navigator.geolocation.watchPosition(
-      // Success callback function
-      function(position) {
-        // Get the user's latitude and longitude coordinates
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-  
-        // Update the map with the user's new location
-        console.log(`Latitude: ${lat}, longitude: ${lng}`);
-  
-        // Update the HTML element with the user's coordinates
-        coordsDiv.innerHTML = `Latitude: ${lat}, longitude: ${lng}`;
-      },
-      // Error callback function
-      function(error) {
-        // Handle errors, e.g. user denied location sharing permissions
-        console.error("Error getting user location:", error);
-  
-        // Display an error message on the HTML element
-        coordsDiv.innerHTML = "Error getting user location: " + error.message;
-      }
-    );
-  } else {
-    // Geolocation is not supported by the browser
-    console.error("Geolocation is not supported by this browser.");
-  
-    // Display an error message on the HTML element
-    coordsDiv.innerHTML = "Geolocation is not supported by this browser.";
-  }
-  
-  // Geolocation is not supported by the browser
+} else {
   console.error("Geolocation is not supported by this browser.");
 }
-
