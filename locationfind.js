cfe51758fb80795b42b66025531d93aa78405ccf
Linda Meng <itsyuko0o1@gmail.com>
2023-12-05 16:07:52 -0800

    a

--- locationfind.js
+++ locationfind.js
@@ -1,5 +1,38 @@
 function getLocation() {
   if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      function(position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        console.log(`Latitude: ${lat}, longitude: ${lng}`);
+        showPosition(position); // Call showPosition to update the coordinates on each change
+      },
+      function(error) {
+        console.error("Error getting user location:", error);
+      },
+      {
+        timeout: 5000,      // 5 seconds timeout
+        maximumAge: 0   // 1 minute maximum age for cached position
+      }
+    );
+  } else {
+    const x = document.getElementById("coords");
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  const x = document.getElementById("coords");
+
+  x.innerHTML = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
+}
+
+window.addEventListener("load", getLocation);
+
+/*
+function getLocation() {
+  if (navigator.geolocation) {
     // Call getCurrentPosition once to get the initial location
     navigator.geolocation.getCurrentPosition(
       function(position) {
@@ -50,3 +83,4 @@ function showPosition(position) {
 }
 
 window.addEventListener("load", getLocation);
+*/
