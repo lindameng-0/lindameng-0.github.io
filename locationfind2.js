dac7eeb42cca5ccc710020ff57a76c079373c5d8
Linda Meng <itsyuko0o1@gmail.com>
2023-12-08 15:29:10 -0800

    a

---
+++ locationfind2.js
@@ -0,0 +1,31 @@
+if (navigator.geolocation) {
+    var location_timeout = setTimeout("geolocFail()", 10000);
+
+    // Use watchPosition instead of getCurrentPosition
+    var watchID = navigator.geolocation.watchPosition(function(position) {
+        clearTimeout(location_timeout);
+
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+
+        // Display the coordinates in the html element with id "coords"
+        var coords = document.getElementById("coords");
+        coords.innerHTML = "Latitude: " + lat + ", Longitude: " + lng;
+
+        geocodeLatLng(lat, lng);
+    }, function(error) {
+        clearTimeout(location_timeout);
+        geolocFail();
+    });
+} else {
+    // Fallback for no geolocation
+    geolocFail();
+}
+
+// Add a function to stop watching the position
+function stopWatch() {
+    if (watchID) {
+        navigator.geolocation.clearWatch(watchID);
+        watchID = null;
+    }
+}
